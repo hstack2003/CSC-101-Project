19d7773dc37c28ef6cacb0d84bbdffb6bde97070
diegomelgoza <drmelgoz@calpoly.edu>
2025-02-21 09:26:51 -0800

    new files

--- main.py
+++ main.py
@@ -7,12 +7,8 @@ restaurants = [Restaurant("Firestone", "Barbecue", 4, 5),
                Restaurant("McConnell's", "Dessert", 3, 4.5)]
 
 def main():
-    print("Hello! Welcome to Pyelp.")
-    print("You can search for a restaurant based on the following:\n")
-
-
-
-
+    ...
+    #say hello with function
     #Options to sort by name or other attributes
     #If statements to discern between commands.
     #Random function
