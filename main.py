c9afeab09abf261943e385bf911e52cef526a2d5
diegomelgoza <drmelgoz@calpoly.edu>
2025-02-20 17:01:51 -0800

    Hello

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ restaurants = [Restaurant("Firestone", "Barbecue", 4, 5, "Lunch"),
                Restaurant("McConnell's", "Dessert", 3, 4.5, "Dessert")]
 
 def main():
+    ...
     #Hello on start
     #Options to sort by name or other attributes
     #If statements to discern between commands.
