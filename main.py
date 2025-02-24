7d4fbb275d41c3106a7bc71b52bfaa4fbf4503b7
diegomelgoza <drmelgoz@calpoly.edu>
2025-02-24 09:19:15 -0800

    cat push

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ restaurants = [Restaurant("Firestone", "Barbecue", 4, 5),
                Restaurant("McConnell's", "Dessert", 3, 4.5)]
 
 def main():
-    ...
+    for letter in list("cat"):
+        print(letter)
     #say hello with function
     #Options to sort by name or other attributes
     #If statements to discern between commands.
