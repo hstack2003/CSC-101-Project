86d33b58ad58279d4eaa0fd9429fc7cba8c55774
Hannah Stackpole <hstackpo@calpoly.edu>
2025-03-07 20:00:48 -0800

    Tests

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import main
 from data import Restaurant
 from main import compile_results, trim_and_find_reps, results_sorting, results_to_text, restaurants
 import unittest
@@ -23,24 +24,42 @@ class Testcases(unittest.TestCase):
         results = compile_results(user_prefs, restaurants)
         self.assertEqual(expected, results)
 
-
-
-
     def test_compile_results_2(self):
-
+        user_prefs = {"name": None, "cuisine": "pizza", "price": '5', "rating": None}
+        expected = {"cuisine": [Restaurant("woodstock", "pizza", 4, 4.5)], "name": [],
+                    "price": [Restaurant("flour house", "italian", 5, 5), Restaurant("the krusty krab", "burgers", 5, 5)], "rating": []}
+        results = compile_results(user_prefs, restaurants)
+        self.assertEqual(expected, results)
 
     def test_trim_and_find_reps_1(self):
-        ...
+        res = {"cuisine": [Restaurant("woodstock", "pizza", 4, 4.5)], "name": [],
+                    "price": [Restaurant("flour house", "italian", 5, 5), Restaurant("the krusty krab", "burgers", 5, 5)], "rating": []}
+        trimmed = []
+        name_reps = {}
+        result = main.trim_and_find_reps(res)
+        expected = ([Restaurant("woodstock", "pizza", 4, 4.5), Restaurant("flour house", "italian", 5, 5), Restaurant("the krusty krab", "burgers", 5, 5)], {'flour house':1, 'the krusty krab':1, 'woodstock':1})
+        self.assertEqual(result, expected)
+
     def test_trim_and_find_reps_2(self):
         ...
 
     def test_results_sorting_1(self):
-        ...
+        matches = {1: [], 2: [], 3: [], 4: []}
+        results_list = [Restaurant("woodstock", "pizza", 4, 4.5), Restaurant("flour house", "italian", 5, 5), Restaurant("the krusty krab", "burgers", 5, 5)]
+        reps = {'flour house':1, 'the krusty krab':1, 'woodstock':1}
+        result = main.results_sorting(results_list, reps)
+        expected = {1: [Restaurant("woodstock", "pizza", 4, 4.5), Restaurant("flour house", "italian", 5, 5), Restaurant("the krusty krab", "burgers", 5, 5)], 2: [], 3: [], 4: []}
+        self.assertEqual(result, expected)
+
     def test_results_sorting_2(self):
         ...
 
 
     def test_results_to_text_1(self):
-        ...
+        matches = {1: [Restaurant("woodstock", "pizza", 4, 4.5), Restaurant("flour house", "italian", 5, 5), Restaurant("the krusty krab", "burgers", 5, 5)], 2: [], 3: [], 4: []}
+        result = main.results_to_text(matches[1], matches[2], matches[3], matches[4])
+
+
+
     def test_results_to_text_2(self):
         ...
